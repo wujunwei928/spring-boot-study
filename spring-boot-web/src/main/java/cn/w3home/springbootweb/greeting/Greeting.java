1af67f11c84cd451f4f5c09391dfe02bbcff73a6
wujunwei <1399952803@qq.com>
2023-06-26 00:18:32 +0800

    spring boot web practice

---
+++ spring-boot-web/src/main/java/cn/w3home/springbootweb/greeting/Greeting.java
@@ -0,0 +1,25 @@
+package cn.w3home.springbootweb.greeting;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        log.debug("Greeting constructor");
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        log.debug("getId: " + id);
+        return id;
+    }
+
+    public String getContent() {
+        log.debug("getContent: " + content);
+        return content;
+    }
+}
---
+++ spring-boot-web/src/main/java/cn/w3home/springbootweb/hello/HelloController.java
@@ -0,0 +1,25 @@
+package cn.w3home.springbootweb.hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    /**
+     * 使用 @Value 注解, 读取Java代码中的环境或应用程序属性值
+     */
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/app_name")
+    public String name() {
+        return appName;
+    }
+}
